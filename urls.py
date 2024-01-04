64e7cb090b41407a9d85bc8c95f43df87c63aaaa
黄向昂 <shawn@chaoo.ltd>
2024-01-04 17:21:56 +0800

    adjust hierachy

--- urls.py
+++ urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from river import views
+import views
 
 urlpatterns = [
     path('adm/', admin.site.urls),
